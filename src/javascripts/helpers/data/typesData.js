2247683c0e1e2bf8e666c91f621b08c07668ad0c
Silvestre Luna <slvtrluna@gmail.com>
2019-05-20 18:03:56 -0500

    wip

--- src/javascripts/helpers/data/typesData.js
+++ src/javascripts/helpers/data/typesData.js
@@ -1,8 +1,4 @@
 import axios from 'axios';
-// import prod from './productsData';
-
-// if typeid === same as the catergorieID
-// then, get the Type.name
 
 const getTypesData = categories => new Promise((resolve, reject) => {
   axios.get('../db/types.json')
@@ -22,25 +18,5 @@ const getTypesData = categories => new Promise((resolve, reject) => {
 
     .catch(err => reject(err));
 });
-
-// const getTypeWithProd = products => new Promise((resolve, reject) => {
-//   axios.get('../db/types.json')
-//     .then((resp) => {
-//       const allTypes = resp.data.types;
-//       const prodTypes = allTypes.map((type) => {
-//         const newProd = type;
-//         for (let i = 0; i < products.length; i += 1) {
-//           if (products[i].type === newProd.id) {
-//             newProd.catName = products[i].name;
-//             newProd.catDesc = products[i].description;
-//           }
-//         }
-//         console.error(newProd);
-//         return newProd;
-//       });
-//       resolve(prodTypes);
-//     })
-//     .catch(err => reject(err));
-// });
 
 export default { getTypesData };
